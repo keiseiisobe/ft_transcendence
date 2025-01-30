7ff9d8323587f4e5e767a0385d4529066ae40ca7
keiseiisobe <keisei.isobe@gmail.com>
2025-01-31 02:19:49 +0900

    added selenium

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,17 @@ ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 WORKDIR /project
 COPY requirements.txt /project/
-RUN pip install -r requirements.txt
-RUN pip install django-friendship
-RUN pip install django-rainbowtests
-RUN python -m pip install Pillow
-RUN python -m pip install -U 'channels[daphne]'
+RUN apt-get update && apt-get install -y chromium \
+    	    	      	      	      	 chromium-driver;
+RUN pip install -r requirements.txt; \
+    pip install -U 'channels[daphne]'; \
+    pip install django-friendship \
+    		django-rainbowtests \
+		Pillow \
+		"selenium >= 4.8.0" \
+		webdriver-manager \
+		chromedriver-binary;
+ENV CHROME_BIN=/usr/bin/chromium
+ENV CHROME_DRIVER=/usr/bin/chromedriver
+ENV PATH=$PATH:/usr/bin
 COPY . /project/
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 # Create your tests here.
 
@@ -36,4 +40,52 @@ class UserModelTests(TestCase):
         password = ""
         with self.assertRaises(ValidationError):
             User.objects.create_user(username=username, password=password)
-            
+
+
+class SeleniumTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        chromedriver_path = "/usr/bin/chromedriver"
+        service = Service(chromedriver_path)
+        options = webdriver.ChromeOptions()
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')        
+        options.add_argument('--user-data-dir=/tmp')        
+        options.add_argument('--headless')
+        cls.selenium = webdriver.Chrome(service=service, options=options)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_signup_and_login(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        self.assertEqual(self.selenium.title, "Pong Game")
+        username = "kisobe"
+        password = "password"
+        
+        # signup
+        signupModalButton = self.selenium.find_element(By.ID, "signup-modal-button")
+        self.assertIn("Signup", signupModalButton.get_attribute("textContent"))
+        signupModalButton.click()
+        signupUsername = self.selenium.find_element(By.CSS_SELECTOR, "#signup-username")
+        signupUsername.send_key(username)
+        signupPassword = self.selenium.find_element(By.CSS_SELECTOR, "#signup-password")
+        signupPassword.send_key(password)
+        signupButton = self.selenium.find_element(By.CSS_SELECTOR, "#signup-button")
+        signupButton.click()
+
+        # login
+        loginUsername = self.selenium.find_element(By.CSS_SELECTOR, "#login-username")
+        loginUsername.send_key(username)
+        loginPassword = self.selenium.find_element(By.CSS_SELECTOR, "#login-password")
+        loginPassword.send_key(password)
+        loginButton = self.selenium.find_element(By.CSS_SELECTOR, "#login-button")
+        loginButton.click()
+
+        header = self.selenium.find_element(By.CSS_SELECTOR, "#loaded-header")
+        self.assertIn("Mypage", header)
