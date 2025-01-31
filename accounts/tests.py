ec7379a94d5a30f0a1ad2d78bfefa9cf430ea150
keiseiisobe <keisei.isobe@gmail.com>
2025-01-31 23:31:24 +0900

    added selenium tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -63,44 +63,190 @@ class SeleniumTest(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_signup_and_login_and_logout(self):
-        self.selenium.get(f"{self.live_server_url}/pong/")
-        self.assertEqual(self.selenium.title, "Pong Game")
-        username = "kisobe"
-        password = "password"
-        
-        # signup
-        signupModalButton = WebDriverWait(self.selenium, 50).until(
-            EC.element_to_be_clickable((By.ID, "signup-modal-button"))
-        )
-        signupModalButton.click()
-        signupUsername = WebDriverWait(self.selenium, 50).until(
+    # utils
+
+    def signup(self, username, password):
+        signupUsername = WebDriverWait(self.selenium, 10).until(
             EC.element_to_be_clickable((By.ID, "signup-username"))
         )
         signupUsername.send_keys(username)
         signupPassword = self.selenium.find_element(By.ID, "signup-password")
         signupPassword.send_keys(password)
+        self.selenium.implicitly_wait(10)
         signupButton = self.selenium.find_element(By.ID, "signup-button")
         signupButton.click()
 
-        # login
-        loginUsername = WebDriverWait(self.selenium, 50).until(
+    def login(self, username, password):
+        loginUsername = WebDriverWait(self.selenium, 10).until(
             EC.element_to_be_clickable((By.ID, "login-username"))
         )
         loginUsername.send_keys(username)
         loginPassword = self.selenium.find_element(By.ID, "login-password")
         loginPassword.send_keys(password)
+        self.selenium.implicitly_wait(10)
         loginButton = self.selenium.find_element(By.ID, "login-button")
         loginButton.click()
 
-        logoutButton = WebDriverWait(self.selenium, 50).until(
+    def logout(self):
+        logoutButton = self.selenium.find_element(By.ID, "logout-button")
+        logoutButton.click()
+        signupModalButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "signup-modal-button"))
+        )
+        self.assertEqual(signupModalButton.get_attribute("id"), "signup-modal-button")
+
+    def signup_prepare(self):
+        signupModalButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "signup-modal-button"))
+        )
+        signupModalButton.click()
+
+    def login_prepare(self):
+        loginModalButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "login-modal-button"))
+        )
+        loginModalButton.click()
+        
+    def signup_ok(self):
+        loginUsername = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "login-username"))
+        )
+
+    def signup_error(self, expected_message):
+        error_message = WebDriverWait(self.selenium, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "signup-error-message"))
+        )
+        self.assertIn(expected_message, error_message.text)
+
+    def login_ok(self):
+        logoutButton = WebDriverWait(self.selenium, 10).until(
             EC.element_to_be_clickable((By.ID, "logout-button"))
         )
         self.assertEqual(logoutButton.get_attribute("value"), "Logout")
 
-        #logout
-        logoutButton.click()
-        signupModalButton = WebDriverWait(self.selenium, 50).until(
-            EC.element_to_be_clickable((By.ID, "signup-modal-button"))
+    def login_error(self, expected_message):
+        error_message = WebDriverWait(self.selenium, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "login-error-message"))
         )
-        self.assertEqual(signupModalButton.get_attribute("id"), "signup-modal-button")
+        self.assertIn(expected_message, error_message.text)
+
+    def signup_close(self):
+        closeButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "signup-close"))
+        )
+        closeButton.click()
+        self.selenium.implicitly_wait(50)
+        
+    def login_close(self):
+        closeButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "login-close"))
+        )
+        closeButton.click()
+        self.selenium.implicitly_wait(50)
+
+    def mypage(self):
+        mypageButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "mypage-button"))
+        )
+        mypageButton.click()
+
+    def mypage_close(self):
+        closeButton = WebDriverWait(self.selenium, 10).until(
+            EC.element_to_be_clickable((By.ID, "mypage-close"))
+        )
+        closeButton.click()
+        
+    #success test
+    
+    def test_signup_login_and_logout(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        self.assertEqual(self.selenium.title, "Pong Game")
+        username = "kisobe"
+        password = "password"
+        self.signup_prepare()
+        self.signup(username, password)
+        self.login(username, password)
+        self.login_ok()
+        self.logout()
+
+    def test_mypage(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        self.assertEqual(self.selenium.title, "Pong Game")
+        username = "jyasukaw"
+        password = "password"
+        self.signup_prepare()
+        self.signup(username, password)
+        self.login(username, password)
+        self.login_ok()
+        self.mypage()
+        displayed_username = WebDriverWait(self.selenium, 10).until(
+            EC.visibility_of_element_located((By.ID, "username"))
+        )
+        self.assertEqual(displayed_username.text, username)
+        self.mypage_close()
+        self.logout()
+        
+    # failure test
+    error_message = {
+        "alreadyfriend": "You have already requested friendship.",
+        "badpassword": "This password is too short",
+        "incorrect": "Username or password is incorrect",
+        "nosuchuser": "No such user.",
+        "usernameempty": "Username cannot be empty",
+        "usernameexist": "Username already exist. Try another username."
+    }
+
+    def test_username_is_empty(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        username = ""
+        password = "I am empty."
+        self.signup_prepare()
+        self.signup(username, password)
+        self.signup_error(self.error_message["usernameempty"])
+
+    def test_password_is_empty(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        username = "baduser"
+        password = ""
+        self.signup_prepare()
+        self.signup(username, password)
+        self.signup_error(self.error_message["badpassword"])
+
+    def test_password_is_shorter_than_5(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        username = "shortpassword"
+        password = "four"
+        self.signup_prepare()
+        self.signup(username, password)
+        self.signup_error(self.error_message["badpassword"])
+
+    def test_login_without_signup(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        username = "notsignupyet"
+        password = "letmelogin"
+        self.login_prepare()
+        self.login(username, password)
+        self.login_error(self.error_message["incorrect"])
+
+    def test_login_with_wrong_password(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        username = "forgotmypassword"
+        password = "abracadabra"
+        self.signup_prepare()
+        self.signup(username, password)
+        self.signup_ok()
+        wrong_password = "abrakatabra"
+        self.login(username, wrong_password)
+        self.login_error(self.error_message["incorrect"])
+
+    def test_signup_with_username_already_present(self):
+        self.selenium.get(f"{self.live_server_url}/pong/")
+        username = "resaito"
+        password = "password"
+        self.signup_prepare()
+        self.signup(username, password)
+        self.signup_ok()
+        self.login_close()
+        self.signup_prepare()
+        self.signup(username, password)
+        self.signup_error(self.error_message["usernameexist"])
