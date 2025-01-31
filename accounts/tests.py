6c1bf6e597b2052ef3866d601760cc8a725235e2
keiseiisobe <keisei.isobe@gmail.com>
2025-01-31 16:51:22 +0900

    fixing selenium

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,6 +7,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 # Create your tests here.
+import os
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'web:8081'
 
 class UserModelTests(TestCase):
     def test_username_must_not_be_empty(self):
@@ -45,7 +47,14 @@ class UserModelTests(TestCase):
 class SeleniumTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
+        cls.host = "localhost"
+        cls.port = 8081
         super().setUpClass()
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        cls.selenium = webdriver.Remote(command_executor="http://selenium:4444/wd/hub", options=options)
+        cls.selenium.implicitly_wait(10)
+        '''
         chromedriver_path = "/usr/bin/chromedriver"
         service = Service(chromedriver_path)
         options = webdriver.ChromeOptions()
@@ -56,14 +65,16 @@ class SeleniumTest(StaticLiveServerTestCase):
         options.add_argument('--headless')
         cls.selenium = webdriver.Chrome(service=service, options=options)
         cls.selenium.implicitly_wait(10)
-
+        '''
+        
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
 
     def test_signup_and_login(self):
-        self.selenium.get(f"{self.live_server_url}/pong/")
+        print(f"{self.live_server_url}")
+        self.selenium.get(f"http://web:8081/pong/")
         self.assertEqual(self.selenium.title, "Pong Game")
         username = "kisobe"
         password = "password"
@@ -72,20 +83,23 @@ class SeleniumTest(StaticLiveServerTestCase):
         signupModalButton = self.selenium.find_element(By.ID, "signup-modal-button")
         self.assertIn("Signup", signupModalButton.get_attribute("textContent"))
         signupModalButton.click()
+        self.selenium.implicitly_wait(10)
         signupUsername = self.selenium.find_element(By.CSS_SELECTOR, "#signup-username")
-        signupUsername.send_key(username)
+        signupUsername.send_keys(username)
         signupPassword = self.selenium.find_element(By.CSS_SELECTOR, "#signup-password")
-        signupPassword.send_key(password)
+        signupPassword.send_keys(password)
         signupButton = self.selenium.find_element(By.CSS_SELECTOR, "#signup-button")
         signupButton.click()
+        self.selenium.implicitly_wait(10)
 
         # login
         loginUsername = self.selenium.find_element(By.CSS_SELECTOR, "#login-username")
-        loginUsername.send_key(username)
+        loginUsername.send_keys(username)
         loginPassword = self.selenium.find_element(By.CSS_SELECTOR, "#login-password")
-        loginPassword.send_key(password)
+        loginPassword.send_keys(password)
         loginButton = self.selenium.find_element(By.CSS_SELECTOR, "#login-button")
         loginButton.click()
 
+        self.selenium.implicitly_wait(10)
         header = self.selenium.find_element(By.CSS_SELECTOR, "#loaded-header")
-        self.assertIn("Mypage", header)
+        self.assertIn("Mypage", header.get_attribute("innerText"))
