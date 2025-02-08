2c06bb3d9777a0d237c0b2bf71269022343e402f
Thomas Choquet <semoir.dense-0h@icloud.com>
2025-02-08 17:35:34 +0900

    remove three from django project and init lib

--- games/settings.py
+++ games/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'three.apps.ThreeConfig',
     'pong.apps.PongConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- games/urls.py
+++ games/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pong/', include("pong.urls")),
-    path('three/', include('three.urls'))
+    path('pong/', include("pong.urls"))
 ]
