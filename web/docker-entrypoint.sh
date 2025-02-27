d057ea46701d3440102db5b70a8edd6c5dbbd94b
resaito <resaito@student.42tokyo.jp>
2025-02-28 00:07:10 +0900

    uwsgiを使ったnginxサーバーの実装

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -2,4 +2,6 @@
 
 ./wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Postgres is up"
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic
+# python manage.py runserver 0.0.0.0:8000
+uwsgi --ini games/uwsgi.ini --module games.wsgi
--- web/games/settings.py
+++ web/games/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "pong-3D/dist/",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
