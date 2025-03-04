732062541cad972806bbcab9320e87a23685b049
szktringoml <szktringoml@gmail.com>
2025-03-05 02:31:28 +0900

    add: prometheus

--- devops/system-monitor/prometheus/tools/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/prometheus/prometheus/releases/download/v2.55.0/prometheus-2.55.0.linux-arm64.tar.gz
-tar -xvf prometheus-2.55.0.linux-arm64.tar.gz
-rm prometheus-2.55.0.linux-arm64.tar.gz
-
-mv /prometheus.yml prometheus-2.55.0.linux-arm64/prometheus.yml
-
-cd prometheus-2.55.0.linux-arm64
-
-./prometheus --config.file=prometheus.yml
