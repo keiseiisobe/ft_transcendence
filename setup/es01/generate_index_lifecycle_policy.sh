376b8d8f6ed57fdf390f9e11a3fb2330b0f25231
szktringoml <szktringoml@gmail.com>
2025-03-21 06:01:06 +0900

    update: apply ILM to log data received from logstash, grafana small fixes

--- setup/es01/generate_index_lifecycle_policy.sh
+++ setup/es01/generate_index_lifecycle_policy.sh
@@ -1,4 +1,3 @@
-CERTS_DIR="/usr/share/elasticsearch/config/certs"
 #create ILM policy
 echo "Creating ILM policy"
 curl -X PUT "https://es01:9200/_ilm/policy/custom_log_policy" \
@@ -11,8 +10,7 @@ curl -X PUT "https://es01:9200/_ilm/policy/custom_log_policy" \
         "hot": {
           "actions": {
             "rollover": {
-              "max_size": "100KB",
-              "max_age": "10m"
+              "max_age": "5m"
             }
           }
         },
@@ -29,24 +27,128 @@ curl -X PUT "https://es01:9200/_ilm/policy/custom_log_policy" \
   }'
 
 # add component_template to logs@custom
-echo "\nadd component_template to logs@custom"
+echo "add component_template to logs@custom"
 curl -X PUT "https://es01:9200/_component_template/logs@custom" \
   --cacert config/certs/ca/ca.crt \
   -u elastic:${ELASTIC_PASSWORD} \
   -H "Content-Type: application/json" \
   -d '{
     "template":{
-      "settings":{
-        "index": {
-          "lifecycle": {
-            "name": "custom_log_policy",
-			"rollover_alias": "logs-postgresql.log-default"
-          }
-        }	
-      },
-	  "lifecycle": {
-    	"data_retention": "2m"
-	  }
+    	"settings":{
+			"number_of_shards": 1,
+    		"number_of_replicas": 0,
+        	"index": {
+          		"lifecycle": {
+            		"name": "custom_log_policy"
+          		}
+        	}	
+    	}
     }
-  }'
+}'	
 
+# create Index Template
+echo "Creating Index Template"
+curl -XPUT -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD} https://es01:9200/_index_template/nginx_index_template -d '
+{
+    "index_patterns": ["logstash-nginx-*"],
+"template": {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0,
+        "index.lifecycle.name": "custom_log_policy",
+		"index.lifecycle.rollover_alias": "logstash-nginx"
+    }
+},
+"priority": 500,
+"composed_of": ["logs@custom"],
+"version": 3,
+"_meta": {
+"description": "my custom"
+}
+}'
+
+curl -XPUT -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD} https://es01:9200/_index_template/postgresql_index_template -d '
+{
+    "index_patterns": ["logstash-postgresql-*"],
+"template": {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0,
+        "index.lifecycle.name": "custom_log_policy",
+		"index.lifecycle.rollover_alias": "logstash-postgresql"
+    }
+},
+"priority": 500,
+"composed_of": ["logs@custom"],
+"version": 3,
+"_meta": {
+"description": "my custom"
+}
+}'
+
+until curl -X GET "https://es01:9200/_index_template/nginx_index_template" --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD}; do
+  sleep 1
+done
+
+echo "create index"
+curl -X PUT "https://es01:9200/logstash-nginx-000001" \
+  --cacert config/certs/ca/ca.crt \
+  -u elastic:${ELASTIC_PASSWORD} \
+  -H "Content-Type: application/json" \
+  -d '{
+  "aliases": {
+	"logstash-nginx": {
+	  "is_write_index": true
+	}
+  }
+}'
+
+until curl -X GET "https://es01:9200/_index_template/postgresql_index_template" --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD}; do
+  sleep 1
+done
+curl -X PUT "https://es01:9200/logstash-postgresql-000001" \
+  --cacert config/certs/ca/ca.crt \
+  -u elastic:${ELASTIC_PASSWORD} \
+  -H "Content-Type: application/json" \
+  -d '{
+  "aliases": {
+	"logstash-postgresql": {
+	  "is_write_index": true
+	}
+  }
+}'
+
+
+
+echo "Create alias"
+curl -XPOST "https://es01:9200/_aliases" \
+  --cacert config/certs/ca/ca.crt \
+  -u elastic:${ELASTIC_PASSWORD} \
+  -H "Content-Type: application/json" \
+  -d '{
+  "actions": [
+	{
+	  "add": {
+		"index": "<logstash-nginx-000001>",
+		"alias": "logstash-nginx",
+		"is_write_index": true
+	  }
+	}
+  ]
+}'
+
+curl -XPOST "https://es01:9200/_aliases" \
+  --cacert config/certs/ca/ca.crt \
+  -u elastic:${ELASTIC_PASSWORD} \
+  -H "Content-Type: application/json" \
+  -d '{
+  "actions": [
+	{
+	  "add": {
+		"index": "logstash-postgresql-000001",
+		"alias": "logstash-postgresql",
+		"is_write_index": true
+	  }
+	}
+  ]
+}'
