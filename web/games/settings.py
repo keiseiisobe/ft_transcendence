387d06e33267e7636f889676f2f0509ebb1ef967
Kouta Suzuki <kousuzuk@c2r1s1.42tokyo.jp>
2025-05-08 23:12:34 +0900

    fix: conflict

--- web/games/settings.py
+++ web/games/settings.py
@@ -183,7 +183,6 @@ DJANGO_VITE = {
     }
 }
 
-<<<<<<< HEAD
 # jwt
 JWT_SECRET_KEY = SECRET_KEY
 JWT_ALGORITHM = 'HS256'
@@ -191,8 +190,6 @@ ACCESS_TOKEN_KEY = 'jwt_access'
 REFRESH_TOKEN_KEY = 'jwt_refresh'
 ACCESS_TOKEN_LIFETIME = datetime.timedelta(minutes=5)
 REFRESH_TOKEN_LIFETIME = datetime.timedelta(days=7)
-=======
 ELASTIC_APM = {
     "SERVICE_NAME": "pong-backend",
 }
->>>>>>> 35565a2 (Restore changes from backup branch)
